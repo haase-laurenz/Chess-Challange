b8c653b957307db49ba48f95c1c9b4cc1dc2aefa
KingLauri200 <laurenz.haase@gmx.de>
2023-11-03 20:57:47 +0100

    new bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,71 +1,14 @@
 ﻿﻿﻿using ChessChallenge.API;
 using System;
-using System.Linq;
-using System.Collections.Generic;
 
 public class MyBot : IChessBot
-{   
-    int maxdepth;
+{  
+
     Move bestmoveRoot = Move.NullMove;
 
     // https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function
-    int[] pieceVal = {0, 100, 310, 330, 500, 1000, 10000 };
-    int[] piecePhase = {0, 0, 1, 1, 2, 4, 0};
-    ulong[] psts = {657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569, 366006824779723922, 366006826859316500, 786039115310605588, 421220596516513823, 366011295806342421, 366006826859316436, 366006896669578452, 162218943720801556, 440575073001255824, 657087419459913430, 402634039558223453, 347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514, 329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 257053881053295759, 291134268204721362, 492947507967247313, 367159395376767958, 384021229732455700, 384307098409076181, 402035762391246293, 328847661003244824, 365712019230110867, 366002427738801364, 384307168185238804, 347996828560606484, 329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047, 347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 384289231362147538, 384307167128540502, 366006826859320596, 366006826876093716, 366002360093332756, 366006824694793492, 347992428333053139, 457508666683233428, 329723156783776785, 329401687190893908, 366002356855326100, 366288301819245844, 329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863, 419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 366855645423297932, 329991151972070674, 311105941360174354, 256772197720318995, 365993560693875923, 258219435335676691, 383730812414424149, 384601907111998612, 401758895947998613, 420612834953622999, 402607438610388375, 329978099633296596, 67159620133902};
-
-    // https://www.chessprogramming.org/Transposition_Table
-    struct TTEntry {
-        public ulong key;
-        public Move move;
-        public int depth, score, bound;
-        public TTEntry(ulong _key, Move _move, int _depth, int _score, int _bound) {
-            key = _key; move = _move; depth = _depth; score = _score; bound = _bound;
-        }
-    }
-
-    const int entries = (1 << 20);
-    TTEntry[] tt = new TTEntry[entries];
-
-    int GetPieceValue(Piece piece)
-    {
-        // Hier kannst du die Wertigkeiten der verschiedenen Figuren anpassen
-        var pieceValues = new Dictionary<PieceType, int>
-        {
-            { PieceType.None, 0 }, // Leeres Feld hat den Wert 0
-            { PieceType.Pawn, 100 },
-            { PieceType.Knight, 300 },
-            { PieceType.Bishop, 320 },
-            { PieceType.Rook, 500 },
-            { PieceType.Queen, 900 },
-            { PieceType.King, 0 }
-        };
-
-        return pieceValues[piece.PieceType];
-    }
-    int GetPieceCount(Board board){
-
-            return board.GetAllPieceLists()
-            .SelectMany(pieceList => pieceList)
-            .Sum(piece => piece.IsWhite ? GetPieceValue(piece) : -GetPieceValue(piece));
-    }
-
-    public int Evaluate(Board board) {
-
-            int score=GetPieceCount(board)+GetPiecePositioning(board);
-            
-            return board.IsWhiteToMove ? score : -score;
-    }
-
-    int GetPiecePositioning(Board board)
-        {
-            int score = 0;
-            
-            ulong allPiecesBitboard = board.AllPiecesBitboard;
-            int totalPieces = BitboardHelper.GetNumberOfSetBits(allPiecesBitboard);
-
-            double factor = (32.0 - totalPieces) / 32.0;
-
-            int[] knights = { -40, -10, -20, -20, -20, -20, -10, -40,
+    int[] pieceVal = {0, 100, 310, 330, 500, 900, 10000 };
+    int[] knights = { -40, -10, -20, -20, -20, -20, -10, -40,
                             -40, -20,  0,  0,  0,  0, -20, -40,
                             -30,  10, 10, 15, 15, 10,  10, -30,
                             -30,  5, 15, 20, 20, 15,  5, -30,
@@ -128,50 +71,77 @@ public class MyBot : IChessBot
                             70, 70, 70,70,70, 70, 70, 70,
                             0,  0,  0,  0,  0,  0,  0,  0 };
 
-            for (int index=0;index<64;index++)
-            {
-                
-                king[index] = (int)(king[index] + (king_end[index] - king[index]) * factor);
-                pawns[index] = (int)(pawns[index] + (pawns_end[index] - pawns[index]) * factor);
-                
-            }           
+    
+    // https://www.chessprogramming.org/Transposition_Table
+    struct TTEntry {
+        public ulong key;
+        public Move move;
+        public int depth, score, bound;
+        public TTEntry(ulong _key, Move _move, int _depth, int _score, int _bound) {
+            key = _key; move = _move; depth = _depth; score = _score; bound = _bound;
+        }
+    }
 
-            foreach (PieceList pieceList in board.GetAllPieceLists())
-            {
-                int[] pieceValues;
+    const int entries = (1 << 20);
+    TTEntry[] tt = new TTEntry[entries];
 
-                switch (pieceList.TypeOfPieceInList)
-                {
-                    case PieceType.Knight:
-                        pieceValues = knights;
-                        break;
-                    case PieceType.Bishop:
-                        pieceValues = bishops;
-                        break;
-                    case PieceType.Rook:
-                        pieceValues = rooks;
-                        break;
-                    case PieceType.King:
-                        pieceValues = king;
-                        break;
-                    case PieceType.Pawn:
-                        pieceValues = pawns;
-                        break;
-                    default:
-                        continue;
+
+    public int Evaluate(Board board) {
+
+            int score=GetPiecePositioning(board);
+            
+            return board.IsWhiteToMove ? score : -score;
+    }
+
+    public int GetPiecePositioning(Board board)
+        {
+            int score = 0;
+            
+            ulong allPiecesBitboard = board.AllPiecesBitboard;
+            int totalPieces = BitboardHelper.GetNumberOfSetBits(allPiecesBitboard);
+
+            double factor = (32.0 - totalPieces) / 32.0;
+
+            
+            foreach(bool stm in new[] {true, false}) {
+                for(var p = PieceType.Pawn; p <= PieceType.King; p++) {
+                    int piece = (int)p;
+                    ulong mask = board.GetPieceBitboard(p, stm);
+                    while(mask != 0) {
+                        int squareIndex = BitboardHelper.ClearAndGetIndexOfLSB(ref mask);
+                        score+=getPieceSquareScore(p,squareIndex,factor)+pieceVal[piece];
+                    }
                 }
-
-                foreach (Piece piece in pieceList)
-                {   
-                    int mirroredRow = 7 - piece.Square.Rank;
-                    int mirroredSquareIndex = mirroredRow * 8 + piece.Square.File;
-                    int pieceValue = piece.IsWhite? pieceValues[piece.Square.Index] : pieceValues[mirroredSquareIndex];
-                    score += piece.IsWhite ? pieceValue : -pieceValue;
-                }
+                score*=-1;
             }
+            
 
             return score;
         }
+    
+    public int getPieceSquareScore(PieceType pieceType,int index,double factor){
+        if (pieceType==PieceType.Pawn){
+            return (int)(pawns[index] + (pawns_end[index] - pawns[index]) * factor);
+        }
+        if (pieceType==PieceType.Knight){
+            return knights[index];
+        } 
+        if (pieceType==PieceType.Bishop){
+            return bishops[index];
+        } 
+        if (pieceType==PieceType.Rook){
+            return rooks[index];
+        } 
+        if (pieceType==PieceType.King){
+            return (int)(king[index] + (king_end[index] - king[index]) * factor);
+        }
+        if (pieceType==PieceType.Queen){
+            return bishops[index]+rooks[index];
+        } 
+
+        Console.WriteLine("NO PIECES ON THE BOARD TO EVALUATE");
+        return 0;
+    }
 
     // https://www.chessprogramming.org/Negamax
     // https://www.chessprogramming.org/Quiescence_Search
